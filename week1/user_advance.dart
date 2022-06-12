fc74e77c94b16ef607cd626ef7572c325d34d8a3
Ramazan Özkan <ramazanzkn07@gmail.com>
2022-06-12 21:07:30 +0300

    methods

--- week1/user_advance.dart
+++ week1/user_advance.dart
@@ -1,1 +1,21 @@
+void main() {
+  const String bankName = 'Bank of Antalya';
+  const String user1 = "bank 1 user";
+// ekran geleceği zaman alınacak bilig için const yerine final kullan.
+  const double user1Money = 100.00;
+  const String user2 = "bank 2 user";
+  int user2Money = 200;
+  user2Money = user2Money - user1Money.toInt();
+  //.toInt() methodu burada double değişkenini int değişkenine dönüştürür kendisi.
 
+  print("user 2 money: $user2Money");
+
+  const int halisaha = 100;
+
+  int mus1 = 20;
+  int mus2 = 50;
+
+  int sum = mus1 + mus2;
+  print("sum: $sum");
+  print("total: ${halisaha - sum}"); // $ da kullanılan paranteze dikkat {}
+}
