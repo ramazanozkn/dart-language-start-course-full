4c9e328183f9dafffe3de08977416724241a2ecd
Ramazan Özkan <ramazanzkn07@gmail.com>
2022-07-04 23:36:55 +0300

    class

--- week4/class.dart
+++ week4/class.dart
@@ -3,6 +3,11 @@ void main() {
   print(user1.name);
 
   User user2 = User('veli', 290, null, null);
+  print(user2.age);
+
+  UserDif user3 = UserDif('ali', 290);
+
+  UserDif user4 = UserDif('elma', 230, age: 22, city: 'afrin');
 }
 
 //adı ve parası zorunlu
@@ -13,11 +18,38 @@ class User {
   late final int money;
   late final int? age; //nullable ? olmayabilir
   late final String? city;
-
+// ? olan kısımlar değer atanabilir veya null olarak atanabilir ama değer yada null demek zorunlu
   User(String name, int money, int? age, String? city) {
     this.name = name;
     this.money = money;
     this.age = age;
     this.city = city;
   }
+}
+
+class UserDif {
+  // classın özellikleri
+  late final String name; //late sonradan değer atanacak
+  late final int money;
+  late final int? age; //nullable ? olmayabilir
+  late final String? city;
+
+  // ? parantez iiçinde olunva ara metod oluşturulur belirtmek zorunda değiliz.
+
+  UserDif(String name, int money, {int? age, String? city}) {
+    this.name = name;
+    this.money = money;
+    this.age = age;
+    this.city = city;
+  }
+}
+
+// (city ?? 'ist) => city değeri null ise ist değeri atanır.
+class User2 {
+  final String name;
+  final int money;
+  final int? age;
+  final String? city;
+
+  User2(this.name, this.money, {this.age, this.city});
 }
